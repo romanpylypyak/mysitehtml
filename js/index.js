501c06e17a13f68db5c1ddf1a76aa08134a551e2
Roman_Pylypyak <roman.lviv@yahoo.com>
2018-12-03 00:48:57 +0000

    Initial

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 setInterval( updateClock, 1000);
 function updateClock() {
     const clock = document.getElementById('clock');
-    clock.innerText = (new Date()).toLocaleTimeString();
+    clock.innerHTML = (new Date()).toLocaleTimeString();
 }    
